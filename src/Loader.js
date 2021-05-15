d946562a19b9c4b429c54c39cb6ac99dae24f21e
Jessica <jessica@jessicas-mbp.lan>
2021-05-15 13:16:58 -0700

    add loader component and styles

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function Loader() {
+  return (
+    <div className="loading fa-8x">
+      <h1>Loading</h1>
+      <i className="fas fa-spinner fa-spin"></i>
+    </div>
+  )
+}
+
+export default Loader;
