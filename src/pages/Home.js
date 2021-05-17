628aa412bb4b3be9b6e6451df2fc633cca76ad80
Jessica <jessica@jessicas-mbp.lan>
2021-05-16 21:13:35 -0700

    add background image for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,20 @@
 import { useContext } from "react";
 import UserContext from "../userContext";
+import "../App.css"
 
+/** Renders / */
 function Home() {
   const { currentUser, token } = useContext(UserContext);
   return (
-    <>
-    <div className="my-3 d-flex justify-content-center">
-      {token ? (
-        <h1>Welcome back, {currentUser.firstName}!</h1>
-      ) : (
-        <h1>Welcome to Jobly!</h1>
-      )}
+    <div className="home-container">
+      <div className="my-3 d-flex justify-content-center">
+        {token ? (
+          <h1>Welcome back, {currentUser.firstName}!</h1>
+        ) : (
+          <h1>Welcome to Jobly!</h1>
+        )}
+      </div>
     </div>
-    </>
   );
 }
 
